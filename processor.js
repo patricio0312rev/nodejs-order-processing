7728a328e8520fddecec26ad5c4f6ccfe0c6aef2
patricio0312rev <review0312oficial@gmail.com>
2022-01-16 21:17:47 -0500

    Creando constructor en processor

--- processor.js
+++ processor.js
@@ -2,7 +2,11 @@ const EventEmitter = require('events');
 const StockList = require('./stock-list.json');
 
 class OrderProcessor extends EventEmitter {
-    
+
+    constructor() {
+        super();
+        this.stock = StockList;
+    }
 }
 
 module.exports = OrderProcessor;
