1639c01db245b6f87cb011f80998cb51ed2c7e93
patricio0312rev <review0312oficial@gmail.com>
2022-01-16 21:18:41 -0500

    Creando emit LINEITEMS_EMPTY

--- processor.js
+++ processor.js
@@ -2,10 +2,25 @@ const EventEmitter = require('events');
 const StockList = require('./stock-list.json');
 
 class OrderProcessor extends EventEmitter {
-
     constructor() {
         super();
         this.stock = StockList;
+    }
+
+
+    placeOrder(payload) {
+        this.emit('PROCESSING_STARTED', payload.orderNumber);
+
+        const items = payload.lineItems;
+
+        if(items && items.length > 0) {
+            
+        } else {
+            this.emit('PROCESSING_FAILED', {
+                orderNumber: payload.orderNumber,
+                reason: 'LINEITEMS_EMPTY',
+            });
+        }
     }
 }
 
